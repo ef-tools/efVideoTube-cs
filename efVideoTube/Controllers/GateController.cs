5bc6d75b0959ff6205d2e8f60f49e8f429da9e0d
Fire <Fire@327c06df-790c-a84e-b573-9b11f8e4cc45>
2012-12-28 06:30:45 +0000

    added client certificate authentication
    git-svn-id: https://eriforce.eicp.net:8443/svn/efVideoTube@14 327c06df-790c-a84e-b573-9b11f8e4cc45

---
+++ efVideoTube/Controllers/GateController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using PureLib.AspNet;
+using PureLib.Common;
+
+namespace efVideoTube.Controllers {
+    public class GateController : Controller {
+        public ActionResult Login(string returnUrl) {
+            if (Request.ClientCertificate.Authenticate(ConfigurationManager.AppSettings["issuer"]))
+                return returnUrl.IsNullOrEmpty() ? RedirectToAction("Index", "Home") : Redirect(returnUrl) as ActionResult;
+            else
+                return new HttpStatusCodeResult((int)HttpStatusCode.Unauthorized);
+        }
+    }
+}
