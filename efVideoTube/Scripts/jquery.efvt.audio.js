67bf506b65842c2a94bc34ee014b24cee2069414
Fire <Fire@327c06df-790c-a84e-b573-9b11f8e4cc45>
2013-08-26 05:21:44 +0000

    added next function for audio player
    git-svn-id: https://eriforce.eicp.net:8443/svn/efVideoTube@58 327c06df-790c-a84e-b573-9b11f8e4cc45

--- efVideoTube/Scripts/jquery.efvt.audio.js
+++ efVideoTube/Scripts/jquery.efvt.audio.js
@@ -7,6 +7,22 @@
         audio.load();
         audio.play();
     };
+    var goNext = function (e) {
+        switch (getCookie('audioLoop')) {
+            case 'single':
+                play();
+                break;
+            case 'all':
+                playlist.find('a#' + index).removeClass(playingClassName);
+                if (getCookie('audioOrder') == 'normal')
+                    index = (index + 1) % list.length;
+                else
+                    index = Math.floor(Math.random() * list.length);
+                window.location.hash = index;
+                play();
+                break;
+        }
+    };
 
     regRadioOptions('audioOrder', null, null);
     regRadioOptions('audioLoop', null, null);
@@ -25,21 +41,10 @@
 
     var playingClassName = 'playing';
     var audio = $('audio').get(0);
-    audio.addEventListener('ended', function (e) {
-        switch (getCookie('audioLoop')) {
-            case 'single':
-                play();
-                break;
-            case 'all':
-                playlist.find('a#' + index).removeClass(playingClassName);
-                if (getCookie('audioOrder') == 'normal')
-                    index = (index + 1) % list.length;
-                else
-                    index = Math.floor(Math.random() * list.length);
-                window.location.hash = index;
-                play();
-                break;
-        }
+    audio.addEventListener('ended', goNext);
+    $('a.next').click(function (e) {
+        e.preventDefault();
+        goNext(e);
     });
     playlist.find('a').click(function (e) {
         e.preventDefault();
