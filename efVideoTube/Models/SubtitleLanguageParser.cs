b9fb1ff014dcba1f727f801ff726720bcac7ca27
Fire <Fire@327c06df-790c-a84e-b573-9b11f8e4cc45>
2013-09-17 09:55:19 +0000

    updated language names
    git-svn-id: https://eriforce.eicp.net:8443/svn/efVideoTube@60 327c06df-790c-a84e-b573-9b11f8e4cc45

--- efVideoTube/Models/SubtitleLanguageParser.cs
+++ efVideoTube/Models/SubtitleLanguageParser.cs
@@ -6,15 +6,15 @@ using System.Web;
 
 namespace efVideoTube.Models {
     public static class SubtitleLanguageParser {
-        public const string DefaultLanguage = sc;
-        private const string sc = "zh-cn";
-        private const string tc = "zh-tw";
-        private const string jp = "ja-jp";
-        private const string en = "en-us";
+        public const string DefaultLanguage = "Default";
+        private const string en = "English";
+        private const string sc = "简体中文";
+        private const string tc = "繁體中文";
+        private const string jp = "日本語";
 
         private static readonly Dictionary<string, string> extLangMaps = new Dictionary<string, string> {
-            { ".sc", sc }, { ".chs", sc },
-            { ".tc", tc }, { ".cht", tc },
+            { ".sc", sc }, { ".chs", sc }, { ".gb", sc },
+            { ".tc", tc }, { ".cht", tc }, { ".big5", tc },
             { ".jp", jp }, { ".jpn", jp },
             { ".en", en }, { ".eng", en },
         };
