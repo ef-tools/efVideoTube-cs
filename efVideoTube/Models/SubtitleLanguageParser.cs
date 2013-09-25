8bd79482a805495309abc224e0571e406ada0672
Fire <Fire@327c06df-790c-a84e-b573-9b11f8e4cc45>
2013-09-25 01:56:09 +0000

    updated default language selection
    git-svn-id: https://eriforce.eicp.net:8443/svn/efVideoTube@62 327c06df-790c-a84e-b573-9b11f8e4cc45

--- efVideoTube/Models/SubtitleLanguageParser.cs
+++ efVideoTube/Models/SubtitleLanguageParser.cs
@@ -6,12 +6,12 @@ using System.Web;
 
 namespace efVideoTube.Models {
     public static class SubtitleLanguageParser {
-        public const string DefaultLanguage = sc;
         private const string en = "English";
         private const string sc = "简体中文";
         private const string tc = "繁體中文";
         private const string jp = "日本語";
 
+        private static readonly string[] langOrder = new string[] { sc, tc, jp, en };
         private static readonly Dictionary<string, string> extLangMaps = new Dictionary<string, string> {
             { ".sc", sc }, { ".chs", sc }, { ".gb", sc },
             { ".tc", tc }, { ".cht", tc }, { ".big5", tc },
@@ -25,7 +25,15 @@ namespace efVideoTube.Models {
                 if (fileNameWithoutExt.EndsWith(extLang.Key, StringComparison.OrdinalIgnoreCase))
                     return extLang.Value;
             }
-            return DefaultLanguage;
+            return langOrder.First();
+        }
+
+        public static string GetDefaultLanguage(this IEnumerable<string> langs) {
+            foreach (string lang in langOrder) {
+                if (langs.Contains(lang))
+                    return lang;
+            }
+            return null;
         }
     }
 }
