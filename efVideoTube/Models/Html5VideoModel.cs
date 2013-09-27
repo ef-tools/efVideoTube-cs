e9c2bbf06de40bb2d3c1a7806769811778ffeadc
Fire <Fire@327c06df-790c-a84e-b573-9b11f8e4cc45>
2013-09-27 02:01:32 +0000

    fixed srclang attribute of video tracks
    git-svn-id: https://eriforce.eicp.net:8443/svn/efVideoTube@65 327c06df-790c-a84e-b573-9b11f8e4cc45

--- efVideoTube/Models/Html5VideoModel.cs
+++ efVideoTube/Models/Html5VideoModel.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 
 namespace efVideoTube.Models {
     public class Html5VideoModel : MediaModel {
-        public Dictionary<string, string> SubtitleLanguages { get; set; }
+        public Dictionary<string, CultureInfo> SubtitleLanguages { get; set; }
         public string Previous { get; set; }
         public string Next { get; set; }
         public string Parent { get; set; }
--- efVideoTube/Models/SubtitleLanguageParser.cs
+++ efVideoTube/Models/SubtitleLanguageParser.cs
@@ -1,25 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Web;
 
 namespace efVideoTube.Models {
     public static class SubtitleLanguageParser {
-        private const string en = "English";
-        private const string sc = "简体中文";
-        private const string tc = "繁體中文";
-        private const string jp = "日本語";
+        private static readonly CultureInfo en = new CultureInfo("en");
+        private static readonly CultureInfo sc = new CultureInfo("zh-hans");
+        private static readonly CultureInfo tc = new CultureInfo("zh-hant");
+        private static readonly CultureInfo jp = new CultureInfo("ja");
 
-        private static readonly string[] langOrder = new string[] { sc, tc, jp, en };
-        private static readonly Dictionary<string, string> extLangMaps = new Dictionary<string, string> {
+        private static readonly CultureInfo[] langOrder = new CultureInfo[] { sc, tc, jp, en };
+        private static readonly Dictionary<string, CultureInfo> extLangMaps = new Dictionary<string, CultureInfo> {
             { ".sc", sc }, { ".chs", sc }, { ".gb", sc },
             { ".tc", tc }, { ".cht", tc }, { ".big5", tc },
             { ".jp", jp }, { ".jpn", jp },
             { ".en", en }, { ".eng", en },
         };
 
-        public static string Parse(string fileName) {
+        public static CultureInfo Parse(string fileName) {
             string fileNameWithoutExt = Path.GetFileNameWithoutExtension(fileName);
             foreach (var extLang in extLangMaps) {
                 if (fileNameWithoutExt.EndsWith(extLang.Key, StringComparison.OrdinalIgnoreCase))
@@ -28,8 +29,8 @@ namespace efVideoTube.Models {
             return langOrder.First();
         }
 
-        public static string GetDefaultLanguage(this IEnumerable<string> langs) {
-            foreach (string lang in langOrder) {
+        public static CultureInfo GetDefaultLanguage(this IEnumerable<CultureInfo> langs) {
+            foreach (CultureInfo lang in langOrder) {
                 if (langs.Contains(lang))
                     return lang;
             }
