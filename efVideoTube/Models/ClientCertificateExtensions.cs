f6e42e55ab9a7995fb8a5af9278574c2688944a7
Erich <eriforce@live.com>
2016-05-31 22:07:54 +0800

    fixed login error when issuer not set

--- efVideoTube/Models/ClientCertificateExtensions.cs
+++ efVideoTube/Models/ClientCertificateExtensions.cs
@@ -11,31 +11,25 @@ using PureLib.Common;
 namespace efVideoTube.Models {
     public static class ClientCertificateExtensions {
         public static bool CertAuth(this HttpRequestBase request, string issuer) {
-            if (Debugger.IsAttached) {
-                FormsAuthentication.SetAuthCookie("Debug", false, request.ApplicationPath);
-                return true;
-            }
+            string userName = ParseUserName(request.ClientCertificate, issuer);
+            if (userName.IsNullOrEmpty())
+                return false;
 
-            HttpClientCertificate cert = request.ClientCertificate;
-            if (ValidateClientCertificate(cert, issuer)) {
-                FormsAuthentication.SetAuthCookie(cert.Subject.Substring("CN=".Length), false, request.ApplicationPath);
-                return true;
-            }
-
-            return false;
+            FormsAuthentication.SetAuthCookie(userName, false, request.ApplicationPath);
+            return true;
         }
 
-        private static bool ValidateClientCertificate(HttpClientCertificate cert, string issuer) {
+        private static string ParseUserName(HttpClientCertificate cert, string issuer) {
+            if (Debugger.IsAttached)
+                return "Debug";
+
             if (issuer.IsNullOrEmpty())
-                return true;
+                return "Guest";
 
-            if ((cert == null) || !cert.IsValid)
-                return false;
+            if (cert != null && cert.IsValid && cert.BinaryIssuer.ToHexString().Equals(issuer, StringComparison.OrdinalIgnoreCase))
+                return cert.Subject.Substring("CN=".Length);
 
-            if (!cert.BinaryIssuer.ToHexString().Equals(issuer, StringComparison.OrdinalIgnoreCase))
-                return false;
-
-            return true;
+            return null;
         }
     }
 }
